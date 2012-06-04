dd46411d82be46d3cebc1124970e97b69b139d14
Mike Voytovich <mikev@duffresearch.com>
2012-06-03 21:29:12 -0700

    part 6

---
+++ hw1/part6.rb
@@ -0,0 +1,28 @@
+class CartesianProduct
+  include Enumerable
+  def initialize(a, b)
+    @a = a
+    @b = b
+  end
+
+  def each
+    @a.each do |a|
+      @b.each do |b|
+        yield [a, b]
+      end
+    end
+  end
+
+end
+
+#c = CartesianProduct.new([:a,:b], [4,5])
+#c.each { |elt| puts elt.inspect }
+# [:a, 4]
+# [:a, 5]
+# [:b, 4]
+# [:b, 5]
+
+c = CartesianProduct.new([:a,:b], [])
+c.each { |elt| puts elt.inspect }
+# (nothing printed since Cartesian product
+# of anything with an empty collection is empty)
