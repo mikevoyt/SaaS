c040773fcd15a8ed0b6d7febf97273b99677707f
Mike Voytovich <mikev@duffresearch.com>
2012-06-03 17:04:10 -0700

    working history

--- hw1/part5.rb
+++ hw1/part5.rb
@@ -3,12 +3,12 @@ class Class
     attr_name = attr_name.to_s # make sure it's a string
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-#    attr_writer attr_name
-    class_eval "def #{attr_name}=(new_value) ; @#{attr_name} = new_value; end"
+    class_eval "def #{attr_name}=(new_value)
+                  @#{attr_name} = new_value
+                  @#{attr_name}_history ||= [nil]
+                  @#{attr_name}_history.push(new_value)
+                end"
   end
-
-#  def setter_with_history=(attr_name)
-# end
 
 end
 
@@ -19,6 +19,10 @@ class Foo
 
 end
 
-f = Foo.new("")
-f.bar = "helloxx"
-p f.bar
+f = Foo.new
+f.bar = 1
+f.bar = 2
+p f.bar_history
+f = Foo.new
+f. bar = 4
+p f.bar_history
