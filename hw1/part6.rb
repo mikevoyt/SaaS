5b2c209b5dc6daa8d2c3fb063a14732c2e09e35e
Mike Voytovich <mikev@duffresearch.com>
2012-06-03 22:13:00 -0700

    thre is some code from quiz 1; clean up later

--- hw1/part6.rb
+++ hw1/part6.rb
@@ -26,3 +26,38 @@ c = CartesianProduct.new([:a,:b], [])
 c.each { |elt| puts elt.inspect }
 # (nothing printed since Cartesian product
 # of anything with an empty collection is empty)
+
+x = [1,2,3]
+x.send :[]=,0,2
+#p x[0] + x.[](1) + x.send(:[],2)
+
+
+class Book
+  attr_accessor :author
+  attr_reader :title
+  attr_writer :comments
+  def initialize(author, title)
+    @author = author
+    @title = title
+    @comments = []
+  end
+end
+
+book = Book.new("Chuck Palahniuk", "Fight Club")
+
+"#{book.title} was written by #{book.author}."
+
+#p book.superclass
+
+movies = [%q{"Aladdin",   "G"},
+          %q{"I, Robot", "PG-13"},
+          %q{"Star Wars","PG"}]
+
+regexp = /"([^"]+)",\s*"([^"]+)"/
+
+movies.each do |movie|
+  movie.match(regexp)
+  title,rating = $1,$2
+  puts title + " " + rating
+
+end
